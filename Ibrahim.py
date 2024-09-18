fc1069e87d655ab04ca822e137fb836ba5fcc98d
ia13597 <ibrahim.b.abed@gmail.com>
2024-09-18 14:19:34 +0400

    Add files via upload

---
+++ Ibrahim.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Title: Data Science Tools and Ecosystem
+
+# ## In this notebook, Data Science Tools and Ecosystem are summarized
+
+# ## Author:
+# Ibrahim
+
+# **Objectives:**
+# 1. List popular languages in Data Science
+# 2. List commonly used libraries in Data Science
+# 3. Evaluate basic arithmetic expressions
+
+# ### Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. C++
+# 3. Julia
+# 4. Java
+
+# #### Some of the commonly used libraries used by Data Scientists include:
+# 1. TensorFlow
+# 2. Matplotlib
+# 3. Keras
+# 4. Pandas
+
+# | Data Science Tools |
+# | ------------------ |
+# | Apache Spark |
+# | Julia |
+# | Tableau |
+# | Apache Hadoop |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python:
+
+# In[1]:
+
+
+# This a simple arithmetic expression to mutiply then add integers: 
+(3*4)+5
+
+
+# In[2]:
+
+
+# This will convert 200 minutes to hours by diving by 60:
+Hours = 200
+Mins = 60
+NewHours = Hours/Mins
+print("NewHours is: ", NewHours)
+
+
+# In[ ]:
+
+
+
+
